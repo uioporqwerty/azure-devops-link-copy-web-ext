e4e277a2d3a5865484cc0f8824c137dc3d23b0ef
uioporqwerty <nitish.sachar@protonmail.com>
2021-08-21 18:47:58 -0700

    setup prettier

--- app/content_script.js
+++ app/content_script.js
@@ -2,53 +2,47 @@
 function getContrast(color) {
   var rgb = color
     .substring(4, color.length - 1)
-    .replace(/ /g, "")
-    .split(",");
-  var yiq =
-    (parseInt(rgb[0]) * 299 + parseInt(rgb[1]) * 587 + parseInt(rgb[2]) * 114) /
-    1000;
-  return yiq >= 128 ? "black" : "white";
+    .replace(/ /g, '')
+    .split(',');
+  var yiq = (parseInt(rgb[0]) * 299 + parseInt(rgb[1]) * 587 + parseInt(rgb[2]) * 114) / 1000;
+  return yiq >= 128 ? 'black' : 'white';
 }
 
 function getLinkImage() {
-  const contrast = getContrast(
-    window.getComputedStyle(document.body).backgroundColor
-  );
-  if (contrast === "black") {
-    return browser.runtime.getURL("images/link-dark.svg");
+  const contrast = getContrast(window.getComputedStyle(document.body).backgroundColor);
+  if (contrast === 'black') {
+    return browser.runtime.getURL('images/link-dark.svg');
   } else {
-    return browser.runtime.getURL("images/link-light.svg");
+    return browser.runtime.getURL('images/link-light.svg');
   }
 }
 
 function getLink() {
-  const linkElement = document.querySelector(
-    ".workitem-info-bar > .info-text-wrapper > .caption"
-  );
-  return `https://dev.azure.com${linkElement.getAttribute("href")}`;
+  const linkElement = document.querySelector('.workitem-info-bar > .info-text-wrapper > .caption');
+  return `https://dev.azure.com${linkElement.getAttribute('href')}`;
 }
 
 function createMessage() {
-  let message = document.createElement("p");
-  message.innerHTML = "Copied!";
-  message.style.display = "none";
-  message.style.marginLeft = "4px";
+  let message = document.createElement('p');
+  message.innerHTML = 'Copied!';
+  message.style.display = 'none';
+  message.style.marginLeft = '4px';
   return message;
 }
 
 function createWorkItemCopyLink(message) {
-  let workItemCopyLink = document.createElement("img");
-  workItemCopyLink.alt = "Copy link to work item";
+  let workItemCopyLink = document.createElement('img');
+  workItemCopyLink.alt = 'Copy link to work item';
   workItemCopyLink.src = getLinkImage();
-  workItemCopyLink.style.cursor = "pointer";
+  workItemCopyLink.style.cursor = 'pointer';
   workItemCopyLink.onclick = () => {
     const link = getLink();
     navigator.clipboard
       .writeText(link)
       .then(() => {
-        message.style.display = "inline";
+        message.style.display = 'inline';
         setTimeout(function () {
-          message.style.display = "none";
+          message.style.display = 'none';
         }, 3000);
         console.log(`Wrote ${link} to clipboard.`);
       })
@@ -62,8 +56,8 @@ function createWorkItemCopyLink(message) {
 }
 
 function getCopyLinkNode(workItemInfoHeader) {
-  let linkContainer = document.createElement("div");
-  linkContainer.id = "link-container";
+  let linkContainer = document.createElement('div');
+  linkContainer.id = 'link-container';
   let message = createMessage();
   let workItemCopyLink = createWorkItemCopyLink(message);
 
@@ -76,8 +70,7 @@ const obs = new MutationObserver(function (mutations, observer) {
   for (var i = 0; i < mutations.length; i++) {
     const mutationRecord = mutations[i];
     if (
-      mutationRecord.target.className ===
-        "workitem-info-bar workitem-header-bar" &&
+      mutationRecord.target.className === 'workitem-info-bar workitem-header-bar' &&
       mutationRecord.addedNodes.length > 0
     ) {
       mutationRecord.target.children[0].appendChild(getCopyLinkNode());
@@ -88,5 +81,5 @@ obs.observe(document.body, {
   childList: true,
   subtree: true,
   attributes: false,
-  characterData: false,
+  characterData: false
 });
